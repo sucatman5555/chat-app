429621a0cf90029d6a13e971ae265d78c00c91b7
sucatman5555 <sucatman5555@gmail.com>
2020-12-14 15:12:41 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
+  # get 'messages/index'
+
   # SSedit
   root to: "messages#index"
+  # SSedit2
+  resources :users, only: [:edit, :update]
 end
