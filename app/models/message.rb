5766188465d118cb03eb9b18e48974a42362a611
sucatman5555 <sucatman5555@gmail.com>
2020-12-15 17:07:59 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,10 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true
+  # validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached? #コメント無し、画像ありで投稿可能にする
+
+  def was_attached?
+    self.image.attached?
+  end
 end
